3331a9bf716e9b81e0962a89ae2ac53d19ccdefa
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-11-19 16:04:12 +0530

    Added more unit tests

--- test/unit/custom-transformations/settlement-report-params.test.js
+++ test/unit/custom-transformations/settlement-report-params.test.js
@@ -1,11 +1,34 @@
 const { getSettlementReportParams } = require('../../../src/custom-transformations/settlement-report-params')
-const { msgWithNoService } = require('../data/sample-events')
+const { msgWithNoService, createSettlementMsg, csCloseSettlementWindowMsg, closeSettlementWindowMsg, updateSettlementByIdMsg } = require('../data/sample-events')
 
 describe('getSettlementReportParams', () => {
-
   it('Message with no service', () => {
     const result = getSettlementReportParams(msgWithNoService)
     expect(result).toEqual({})
+  })
+
+  it('CreateSettlement message', () => {
+    const result = getSettlementReportParams(createSettlementMsg)
+    expect(result).toHaveProperty('settlementId')
+    expect(result.settlementId).toEqual(createSettlementMsg.metadata.trace.tags?.transactionId)
+  })
+
+  it('cs_close_settlement_window message', () => {
+    const result = getSettlementReportParams(csCloseSettlementWindowMsg)
+    expect(result).toHaveProperty('settlementWindowId')
+    expect(result.settlementWindowId).toEqual(csCloseSettlementWindowMsg.metadata.trace.tags?.transactionId)
+  })
+
+  it('closeSettlementWindow message', () => {
+    const result = getSettlementReportParams(closeSettlementWindowMsg)
+    expect(result).toHaveProperty('settlementWindowId')
+    expect(result.settlementWindowId).toEqual('6')
+  })
+
+  it('updateSettlementById message', () => {
+    const result = getSettlementReportParams(updateSettlementByIdMsg)
+    expect(result).toHaveProperty('settlementId')
+    expect(result.settlementId).toEqual('5')
   })
 
 })
