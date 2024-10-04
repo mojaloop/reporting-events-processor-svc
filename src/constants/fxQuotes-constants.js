33e0687269566c214f51a93d359472e5bfacc9ab
Ujjwal-Izyane <ujjwal.panwar@izyane.com>
2024-10-04 11:36:11 +0530

    Update fxQuotesConstants

--- src/constants/fxQuotes-constants.js
+++ src/constants/fxQuotes-constants.js
@@ -1,3 +1,9 @@
-const fxQuotesConstants = []
+const fxQuotesConstants = [
+  'qs_quote_forwardFxQuoteUpdate',
+  'qs_quote_forwardFxQuoteError',
+  'qs_quote_forwardFxQuoteGet',
+  'qs_fxquote_forwardFxQuoteRequest',
+  'qs_fxQuote_sendErrorCallback',
+]
 
 module.exports = { fxQuotesConstants }
