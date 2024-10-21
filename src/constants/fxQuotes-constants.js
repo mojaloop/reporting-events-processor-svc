5c70023c870d18f7943e0b03fd64d19570206cf2
v3-Panwar-Ujjwal <v3.panwarujjwal@gmail.com>
2024-10-21 16:10:54 +0530

    Remove qs_fxquote message constant from fxQuote constants

--- src/constants/fxQuotes-constants.js
+++ src/constants/fxQuotes-constants.js
@@ -3,7 +3,7 @@ const fxQuotesConstants = [
   'qs_quote_forwardFxQuoteError',
   'qs_quote_forwardFxQuoteGet',
 
-  'qs_fxquote_forwardFxQuoteRequest',
+  // 'qs_fxquote_forwardFxQuoteRequest',
 
   'qs_fxQuote_forwardFxQuoteRequest',
   'qs_fxQuote_sendErrorCallback',
