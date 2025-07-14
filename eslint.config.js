508c19047f8d681b06bb89e72c32318669ba8d5a
Yevhen Kyriukha <kirgene@gmail.com>
2025-07-14 15:15:15 +0300

    chore: bump deps

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const js = require('@eslint/js')
+
+module.exports = [
+  {
+    ignores: [
+      'coverage/**',
+      'templates/**',
+      'docs/**',
+      'examples/**',
+      '**/node_modules/**',
+      '**/*.test.js'
+    ]
+  },
+  {
+    ...js.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'commonjs',
+      globals: {
+        ...require('globals').node
+      }
+    },
+    rules: {
+      'no-console': 'off',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'linebreak-style': ['error', 'unix'],
+      'semi': ['error', 'never']
+    }
+  }
+] 
